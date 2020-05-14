f4ab64ea9d109d8013d1fcf4de50874519f9b294
Alex <maeleeuxs@gmail.com>
2020-05-14 18:24:31 +0200

    Changed API Startup to use ENV variables

--- API/IkKoopMijnEnergieAPI/Startup.cs
+++ API/IkKoopMijnEnergieAPI/Startup.cs
@@ -26,9 +26,18 @@ namespace IkKoopMijnEnergieAPI
         
         public void ConfigureServices(IServiceCollection services)
         {
+            var server = Configuration["DBServer"] ?? "localhost";
+            var port = Configuration["DBPort"] ?? "1433";
+            var user = Configuration["DBUser"] ?? "sa";
+            var password = Configuration["DBPassword"] ?? "Pa$$w0rd2020";
+            var database = Configuration["Database"] ?? "IkmeDB";
+
             services.AddDbContext<IkmeDbContext>(
-                options => options.UseSqlServer(
-                    Configuration.GetConnectionString("LocalConnection")));
+                options => options.UseSqlServer($"Server={server},{port};Initial Catalog={database};User ID={user};Password={password}"));
+
+            //services.AddDbContext<IkmeDbContext>(
+            //    options => options.UseSqlServer(
+            //        Configuration.GetConnectionString("LocalConnection")));
 
             services.AddCors(options =>
             {   
